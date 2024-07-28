29bc585db8b0abd0ed7a108b3043bc27353d1c12
CA-luzgin_ea <luzgin-ea@cityair.ru>
2024-07-28 22:56:42 +0700

    removed default app files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService  } from './app.service';
 import { FlowersModule } from './flowers/flowers.module';
 
 
 @Module({
   imports: [FlowersModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
